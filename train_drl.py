0321d1aa409c2dbbed8b22cb0919860467ff5027
KChenDu <kelvenchendu@gmail.com>
2021-07-20 17:26:21 -0300

    added evaluate_dqn + test push from Chen

--- train_drl.py
+++ train_drl.py
@@ -5,8 +5,9 @@ from dqn_agent import ImageAgent, RAMAgent
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 
-# env = gym.make('SpaceInvaders-ram-v0')
-env = gym.make('SpaceInvaders-v0')
+env = gym.make('SpaceInvaders-ram-v0')
+env.reset()
+# env = gym.make('SpaceInvaders-v0')
 state_size = env.observation_space.shape[0]
 action_size =env.action_space.n
 
